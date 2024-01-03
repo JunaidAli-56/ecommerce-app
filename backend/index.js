1ac96b7ab6df2087e474f1698aa2ea593caff455
Junaid <junaidhayat33303@gmail.com>
2024-01-04 00:39:55 +0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 4000;
+
+
+app.use();
+
+app.listen(port, () => {
+    console.log(`server on port no: ${port}`)
+})
