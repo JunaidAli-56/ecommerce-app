3254ef2dd4f50e7d03e439f971e0ca4642efdcdc
Junaid <junaidhayat33303@gmail.com>
2024-01-04 03:01:25 +0500

    another commit

---
+++ backend/src/config/jwtToken.js
@@ -0,0 +1,6 @@
+const jwt = require('jsonwebtoken')
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '3d' })
+}
+module.exports = generateToken;
--- backend/src/controllers/userCtrl.js
+++ backend/src/controllers/userCtrl.js
@@ -1,3 +1,4 @@
+const generateToken = require('../config/jwtToken');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler')
 
@@ -18,7 +19,15 @@ const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     const findUser = await User.findOne({ email });
     if (findUser && await findUser.isPasswordMatched(password)) {
-        res.json(findUser)
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+            
+        })
     } else {
         // informtaion not match
         throw new Error("invalid credentials")
