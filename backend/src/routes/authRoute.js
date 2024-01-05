dda9b997b99e8fa7b46e25171898a25d8cdb9de8
Junaid <junaidhayat33303@gmail.com>
2024-01-05 11:23:42 +0500

    another commit

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { createUser, loginUser, getAllUser, getSingleUser, deleteUser, updateUser } = require('../controllers/userCtrl');
-const authMiddleware = require('../middleware/authHandler');
+const { authMiddleware, adminHandler } = require('../middleware/authHandler');
 
 const router = express.Router();
 
 router.post('/register', createUser)
 router.post('/login', loginUser)
 router.get('/all-users', getAllUser)
-router.get('/:id', authMiddleware, getSingleUser)
+router.get('/:id', authMiddleware, adminHandler, getSingleUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
 
