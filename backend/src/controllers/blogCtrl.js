581f31197430d3fa5bc7a1ca0bb808cac4add063
Junaid <junaidhayat33303@gmail.com>
2024-01-08 20:56:12 +0500

    another commit

--- backend/src/controllers/blogCtrl.js
+++ backend/src/controllers/blogCtrl.js
@@ -1,2 +1,13 @@
 const asyncHander = require('express-async-handler');
 const Blog = require('../models/blogModel')
+const User = require('../models/userModel');
+const validateMongoId = require('../utils/validateMongoDBId');
+
+
+const createBlog = asyncHander(async (req, res) => {
+
+})
+
+module.exports = {
+    createBlog,
+}
